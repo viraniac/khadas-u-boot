712064b072b5b2fb07827a970b64379d06fd0f08
Meng yu <meng.yu@amlogic.com>
2024-01-01 10:38:11 +0800

    uboot: Clear __asmeq for smc function call 31_apis [4/7]
    PD#SWPL-150262
    
    Problem:
    Clear __asmeq for smc function call in uboot 2019
    
    Solution:
    clear __asmeq in a1 a4 a5 arch
    
    Verify:
    a5 av400
    
    Change-Id: I7ffab7452beee7dd12e48729d363f66d0647b98d
    Signed-off-by: Meng yu <meng.yu@amlogic.com>

--- arch/arm/mach-meson/a1/bl31_apis.c
+++ arch/arm/mach-meson/a1/bl31_apis.c
@@ -13,18 +13,18 @@
 #include <asm/arch/bl31_apis.h>
 #include <amlogic/cpu_id.h>
 #include <asm/arch/secure_apb.h>
+#include <linux/arm-smccc.h>
 
 static long sharemem_input_base;
 static long sharemem_output_base;
 
 long get_sharemem_info(unsigned long function_id)
 {
-	asm volatile(
-		__asmeq("%0", "x0")
-		"smc    #0\n"
-		: "+r" (function_id));
+	struct arm_smccc_res res;
 
-	return function_id;
+	arm_smccc_smc(function_id, 0, 0, 0, 0, 0, 0, 0, &res);
+
+	return res.a0;
 }
 
 int32_t set_boot_params(const keymaster_boot_params *boot_params)
@@ -40,6 +40,7 @@ int32_t meson_trustzone_efuse(struct efuse_hal_api_arg *arg)
 	int ret;
 	unsigned cmd, offset, size;
 	unsigned long *retcnt = (unsigned long *)(arg->retcnt_phy);
+	struct arm_smccc_res res;
 
 	if (!sharemem_input_base)
 		sharemem_input_base =
@@ -64,21 +65,10 @@ int32_t meson_trustzone_efuse(struct efuse_hal_api_arg *arg)
 	}
 	asm __volatile__("" : : : "memory");
 
-	register uint64_t x0 asm("x0") = cmd;
-	register uint64_t x1 asm("x1") = offset;
-	register uint64_t x2 asm("x2") = size;
-	do {
-		asm volatile(
-		    __asmeq("%0", "x0")
-		    __asmeq("%1", "x0")
-		    __asmeq("%2", "x1")
-		    __asmeq("%3", "x2")
-		    "smc    #0\n"
-		    : "=r"(x0)
-		    : "r"(x0), "r"(x1), "r"(x2));
-	} while (0);
-	ret = x0;
-	*retcnt = x0;
+	arm_smccc_smc(cmd, offset, size, 0, 0, 0, 0, 0, &res);
+
+	ret = res.a0;
+	*retcnt = res.a0;
 
 	if ((arg->cmd == EFUSE_HAL_API_READ) && (ret != 0)) {
 		flush_cache(sharemem_output_base, ret);
@@ -96,23 +86,15 @@ int32_t meson_trustzone_efuse_get_max(struct efuse_hal_api_arg *arg)
 {
 	int32_t ret;
 	unsigned cmd = 0;
+	struct arm_smccc_res res;
 
 	if (arg->cmd == EFUSE_HAL_API_USER_MAX)
 		cmd = EFUSE_USER_MAX;
+	else
+		return -1;
 
-	asm __volatile__("" : : : "memory");
-
-	register uint64_t x0 asm("x0") = cmd;
-
-	do {
-		asm volatile(
-		    __asmeq("%0", "x0")
-		    __asmeq("%1", "x0")
-		    "smc    #0\n"
-		    : "=r"(x0)
-		    : "r"(x0));
-	} while (0);
-	ret = x0;
+	arm_smccc_smc(cmd, 0, 0, 0, 0, 0, 0, 0, &res);
+	ret = res.a0;
 
 	if (!ret)
 		return -1;
@@ -141,8 +123,8 @@ ssize_t meson_trustzone_efuse_writepattern(const char *buf, size_t count)
 
 uint64_t meson_trustzone_efuse_check(unsigned char *addr)
 {
-	uint64_t ret = 0;
 	struct sram_hal_api_arg arg = {};
+	struct arm_smccc_res res;
 
 	arg.cmd = SRAM_HAL_API_CHECK_EFUSE;
 	arg.req_len = 0x1000000;
@@ -150,33 +132,17 @@ uint64_t meson_trustzone_efuse_check(unsigned char *addr)
 	arg.req_phy_addr = (unsigned long)addr;
 	arg.res_phy_addr = (unsigned long)NULL;
 
-	asm __volatile__("" : : : "memory");
+	arm_smccc_smc(CALL_TRUSTZONE_HAL_API, TRUSTZONE_HAL_API_SRAM,
+					(unsigned long)(&arg), 0, 0, 0, 0, 0, &res);
 
-	register uint64_t x0 asm("x0") = CALL_TRUSTZONE_HAL_API;
-	register uint64_t x1 asm("x1") = TRUSTZONE_HAL_API_SRAM;
-	register uint64_t x2 asm("x2") = (unsigned long)(&arg);
-	do {
-		asm volatile(
-		    __asmeq("%0", "x0")
-		    __asmeq("%1", "x0")
-		    __asmeq("%2", "x1")
-		    __asmeq("%3", "x2")
-		    "smc #0\n"
-		    : "=r"(x0)
-		    : "r"(x0), "r"(x1), "r"(x2));
-	} while (0);
-
-	ret = x0;
-
-	return ret;
+	return res.a0;
 }
 
 void debug_efuse_cmd(unsigned long cmd)
 {
-	asm volatile(
-		__asmeq("%0", "x0")
-		"smc    #0\n"
-		: : "r" (cmd));
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(cmd, 0, 0, 0, 0, 0, 0, 0, &res);
 }
 
 void bl31_debug_efuse_write_pattern(const char *buf)
@@ -202,125 +168,70 @@ void bl31_debug_efuse_read_pattern(char *buf)
 void aml_set_jtag_state(unsigned state, unsigned select)
 {
 	uint64_t command;
+	struct arm_smccc_res res;
+
 	if (state == JTAG_STATE_ON)
 		command = JTAG_ON;
 	else
 		command = JTAG_OFF;
-	asm __volatile__("" : : : "memory");
-
-	asm volatile(
-		__asmeq("%0", "x0")
-		__asmeq("%1", "x1")
-		"smc    #0\n"
-		: : "r" (command), "r"(select));
+	arm_smccc_smc(command, select, 0, 0, 0, 0, 0, 0, &res);
 }
 
 void wdt_send_cmd_to_bl31(uint64_t cmd, uint64_t value)
 {
-	register long x0 asm("x0") = 0x82000086;
-	register long x1 asm("x1") = cmd;
-	register long x2 asm("x2") = value;
-	register long x3 asm("x3") = 0;
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			__asmeq("%2", "x2")
-			__asmeq("%3", "x3")
-			"smc	#0\n"
-		: "+r" (x0)
-		: "r" (x1), "r" (x2), "r" (x3));
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(0x82000086, cmd, value, 0, 0, 0, 0, 0, &res);
 }
 
 unsigned aml_get_reboot_reason(void)
 {
-	unsigned reason;
-	uint64_t ret;
+	unsigned int reason;
+	struct arm_smccc_res res;
 
-	register uint64_t x0 asm("x0") = GET_REBOOT_REASON;
-	asm volatile(
-		__asmeq("%0", "x0")
-		"smc #0\n"
-		:"+r"(x0));
-		ret = x0;
-		reason = (unsigned)(ret&0xffffffff);
-		return reason;
+	arm_smccc_smc(GET_REBOOT_REASON, 0, 0, 0, 0, 0, 0, 0, &res);
+
+	reason = (unsigned int)(res.a0 & 0xffffffff);
+
+	return reason;
 }
 
 void set_viu_probe_enable(void)
 {
-	register uint64_t x0 asm("x0") = VIU_PREOBE_EN;
+	struct arm_smccc_res res;
 
-	asm volatile(
-			__asmeq("%0", "x0")
-			"smc #0\n"
-			:"+r"(x0));
+	arm_smccc_smc(VIU_PREOBE_EN, 0, 0, 0, 0, 0, 0, 0, &res);
 }
 
 void power_set_dsp(unsigned int id, unsigned int powerflag)
 {
-	register long x0 asm("x0") = DSP_SEC_POWERSET;
-	register long x1 asm("x1") = id;
-	register long x2 asm("x2") = powerflag;
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			__asmeq("%2", "x2")
-			"smc	#0\n"
-		: "+r" (x0)
-		: "r" (x1), "r" (x2));
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(DSP_SEC_POWERSET, id, powerflag, 0, 0, 0, 0, 0, &res);
 }
 
 void init_dsp(unsigned int id,unsigned int addr,unsigned int cfg0)
 {
-	register long x0 asm("x0") = START_HIFI4;
-	register long x1 asm("x1") = id;
-	register long x2 asm("x2") = addr;
-	register long x3 asm("x3") = cfg0;
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			__asmeq("%2", "x2")
-			__asmeq("%3", "x3")
-			"smc	#0\n"
-		: "+r" (x0)
-		: "r" (x1), "r" (x2), "r" (x3));
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(START_HIFI4, id, addr, cfg0, 0, 0, 0, 0, &res);
 }
 
 
 unsigned aml_reboot(uint64_t function_id, uint64_t arg0, uint64_t arg1, uint64_t arg2)
 {
-	register long x0 asm("x0") = function_id;
-	register long x1 asm("x1") = arg0;
-	register long x2 asm("x2") = arg1;
-	register long x3 asm("x3") = arg2;
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			__asmeq("%2", "x2")
-			__asmeq("%3", "x3")
-			"smc	#0\n"
-		: "+r" (x0)
-		: "r" (x1), "r" (x2), "r" (x3));
+	struct arm_smccc_res res;
 
-	return function_id;
+	arm_smccc_smc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+
+	return (unsigned int)res.a0;
 }
 
 void aml_set_reboot_reason(uint64_t function_id, uint64_t arg0, uint64_t arg1, uint64_t arg2)
 {
-	register long x0 asm("x0") = function_id;
-	register long x1 asm("x1") = arg0;
-	register long x2 asm("x2") = arg1;
-	register long x3 asm("x3") = arg2;
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			__asmeq("%2", "x2")
-			__asmeq("%3", "x3")
-			"smc	#0\n"
-			: "+r" (x0)
-			: "r" (x1), "r" (x2), "r" (x3));
+	struct arm_smccc_res res;
 
-	return ;
+	arm_smccc_smc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
 }
 
 unsigned long aml_sec_boot_check(unsigned long nType,
@@ -329,6 +240,7 @@ unsigned long aml_sec_boot_check(unsigned long nType,
 	unsigned long nOption)
 {
 	uint64_t ret = 1;
+	struct arm_smccc_res res;
 
 //#define AML_SECURE_LOG_TE
 
@@ -341,28 +253,8 @@ unsigned long aml_sec_boot_check(unsigned long nType,
 
 	AML_GET_TE(nT1);
 
-	asm __volatile__("" : : : "memory");
-
-	register uint64_t x0 asm("x0") = AML_DATA_PROCESS;
-	register uint64_t x1 asm("x1") = nType;
-	register uint64_t x2 asm("x2") = pBuffer;
-	register uint64_t x3 asm("x3") = nLength;
-	register uint64_t x4 asm("x4") = nOption;
-
-	do {
-		asm volatile(
-		    __asmeq("%0", "x0")
-		    __asmeq("%1", "x0")
-		    __asmeq("%2", "x1")
-		    __asmeq("%3", "x2")
-		    __asmeq("%4", "x3")
-		    __asmeq("%5", "x4")
-		    "smc #0\n"
-		    : "=r"(x0)
-		    : "r"(x0), "r"(x1), "r"(x2),"r"(x3),"r"(x4));
-	} while (0);
-
-	ret = x0;
+	arm_smccc_smc(AML_DATA_PROCESS, nType, pBuffer, nLength, nOption, 0, 0, 0, &res);
+	ret = res.a0;
 
 	AML_GET_TE(nT2);;
 
@@ -380,27 +272,16 @@ unsigned long aml_sec_boot_check(unsigned long nType,
 
 void set_usb_boot_function(unsigned long command)
 {
-	register long x0 asm("x0") = SET_USB_BOOT_FUNC;
-	register long x1 asm("x1") = command;
+	struct arm_smccc_res res;
 
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			"smc	#0\n"
-		: "+r" (x0)
-		: "r" (x1));
+	arm_smccc_smc(SET_USB_BOOT_FUNC, command, 0, 0, 0, 0, 0, 0, &res);
 }
 
 void set_boot_first_timeout(uint64_t arg0)
 {
-	register long x0 asm("x0") = SET_BOOT_FIRST;
-	register long x1 asm("x1") = arg0;
-	asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-			"smc	#0\n"
-			: "+r" (x0)
-			: "r" (x1));
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(SET_BOOT_FIRST, arg0, 0, 0, 0, 0, 0, 0, &res);
 }
 void aml_system_off(void)
 {
@@ -411,6 +292,8 @@ void aml_system_off(void)
 
 int __get_chip_id(unsigned char *buff, unsigned int size)
 {
+	struct arm_smccc_res res;
+
 	if (buff == NULL || size < 16)
 		return -1;
 
@@ -419,23 +302,14 @@ int __get_chip_id(unsigned char *buff, unsigned int size)
 			get_sharemem_info(GET_SHARE_MEM_OUTPUT_BASE);
 
 	if (sharemem_output_base) {
-		register long x0 asm("x0") = GET_CHIP_ID;
-		register long x1 asm("x1") = 2;
+		arm_smccc_smc(GET_CHIP_ID, 2, 0, 0, 0, 0, 0, 0, &res);
 
-		asm volatile(
-			__asmeq("%0", "x0")
-			__asmeq("%1", "x1")
-				"smc	#0\n"
-			: "+r" (x0)
-			: "r" (x1));
-
-		if (x0 == 0) {
+		if (res.a0 == 0) {
 			int version = *((unsigned int *)sharemem_output_base);
 
 			if (version == 2) {
 				memcpy(buff, (void *)sharemem_output_base + 4, 16);
-			}
-			else {
+			} else {
 				/**
 				 * Legacy 12-byte chip ID read out, transform data
 				 * to expected order format.
@@ -467,6 +341,7 @@ int __get_chip_id(unsigned char *buff, unsigned int size)
 int bl31_get_cornerinfo(uint8_t *outbuf, int size)
 {
 	int buff_len = 0;
+	struct arm_smccc_res res;
 
 	if (outbuf == NULL) {
 		printf("BL33: corner efuse info storebuf is NULL\n");
@@ -478,18 +353,9 @@ int bl31_get_cornerinfo(uint8_t *outbuf, int size)
 			get_sharemem_info(GET_SHARE_MEM_OUTPUT_BASE);
 
 	if (sharemem_output_base) {
-		register long x0 asm("x0") = OSCRING_EFUSE_GET;
-		register long x1 asm("x1") = size;
+		arm_smccc_smc(OSCRING_EFUSE_GET, size, 0, 0, 0, 0, 0, 0, &res);
+		buff_len = res.a0;
 
-		do {
-			asm volatile(
-				__asmeq("%0", "x0")
-				__asmeq("%1", "x1")
-				"smc	#0\n"
-				: "+r" (x0)
-				: "r" (x1));
-		} while(0);
-		buff_len = x0;
 		if (buff_len <= size) {
 			memcpy(outbuf, (void *)sharemem_output_base, buff_len);
 			return 0;

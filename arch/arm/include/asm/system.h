248165eb97a70067dfa70d4c423ee272f67d46e0
Meng yu <meng.yu@amlogic.com>
2024-01-08 18:56:38 +0800

    uboot: Clear __asmeq for smc function call 31_apis [7/7]
    PD#SWPL-150262
    
    Problem:
    Clear __asmeq for smc function call in uboot 2019
    
    Solution:
    clear __asmeq in s4 s5 sc2 arch
    
    Verify:
    s4_aq222/s5_ax201/sc2_ah212
    
    Change-Id: Ide9e836a1d6fef0735d0e8b16af6452c4ea28125
    Signed-off-by: Meng yu <meng.yu@amlogic.com>

--- arch/arm/include/asm/system.h
+++ arch/arm/include/asm/system.h
@@ -329,7 +329,6 @@ void psci_arch_init(void);
  * will cause compilation to stop on mismatch.
  * (for details, see gcc PR 15089)
  */
-#define __asmeq(x, y)  ".ifnc " x "," y " ; .err ; .endif\n\t"
 
 #ifndef __ASSEMBLY__
 

da0f0608830e74989e2d4dbeba2ee3c5eabbea48
Yao Jie <jie.yao@amlogic.com>
2024-01-13 16:06:03 +0800

    bl33: t5m add mailbox retry and aocpu alive mechanism [1/1]
    PD#SWPL-155395
    
    Problem:
    If AOCPU does not response or lost interrupt signal
    when BL33 send mailbox message to AOCPU, BL33 will
    stuck and system can not continue running, meanwhile
    BL33 has no method to know whether the mailbox interrupt
    signal is lost or AOCPU is stuck at this time.
    
    Solution:
    Add mailbox timeout and send retry mechanism in mailbox
    send API, moreover, add AOCPU status check mechanism
    in mailbox send API. When BL33 send mailbox to AOCPU
    fail, BL33 will check and print AOCPU status and try
    to resend the same mailbox message twice, if it still
    fail, the API return error directly without block.
    
    Verify:
    T5M-AY309
    
    Change-Id: I00f17b84d9ccd125d5405a8dce2d4168df90d6f1
    Signed-off-by: Yao Jie <jie.yao@amlogic.com>

--- arch/arm/mach-meson/t5m/mailbox.c
+++ arch/arm/mach-meson/t5m/mailbox.c
@@ -1,21 +1,8 @@
-
+// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
 /*
- * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
 #include <config.h>
 #include <common.h>
 #include <asm/arch/io.h>
@@ -63,6 +50,43 @@ static inline void mbclean(uint32_t to, long count)
 		len--;
 		i++;
 	}
+}
+
+/* Return if AOCPU is alive by checking AOCPU tick count */
+static uint32_t is_aocpu_alive(void)
+{
+	uint32_t aocpu_tick_count_1;
+	uint32_t aocpu_tick_count_2;
+	uint32_t ret;
+
+	aocpu_tick_count_1 = aml_readl32(AOCPU_TICK_CNT_RD_ADDR);
+	printf("%s: aocpu_tick_count_1 = %x\n", __func__, aocpu_tick_count_1);
+	/* Wait for 50 msec to let tick count update */
+	mdelay(50);
+	aocpu_tick_count_2 = aml_readl32(AOCPU_TICK_CNT_RD_ADDR);
+	printf("%s: aocpu_tick_count_2 = %x\n", __func__, aocpu_tick_count_2);
+	if (aocpu_tick_count_1 == aocpu_tick_count_2)
+		ret = 0;
+	else
+		ret = 1;
+
+	return ret;
+}
+
+/* Return timerE value (seconds) */
+static uint32_t read_timer_e(void)
+{
+	unsigned long long time64 = 0, time_high = 0;
+	uint32_t time = 0;
+
+	/*timeE high+low, first read low, second read high*/
+	time64 = aml_readl32(TIMERE_LOW_REG);
+	time_high = aml_readl32(TIMERE_HIG_REG);
+	time64 += (time_high << 32);
+	time64 = time64 / 1000000;
+	time = (uint32_t)time64;
+
+	return time;
 }
 
 int mhu_get_addr(uint32_t chan, uint32_t *mboxset, uint32_t *mboxsts,
@@ -126,30 +150,53 @@ void mhu_message_send(uint32_t mboxset, uint32_t command, uint32_t size)
 	aml_writel32(mbox_cmd, mboxset);
 }
 
-uint32_t mhu_message_wait(uint32_t mboxsts)
+uint32_t mhu_message_wait(uint32_t mboxset, uint32_t mboxsts,
+	uint32_t command, uint32_t size)
 {
 	/* Wait for response from other core */
 	uint32_t response;
+	uint32_t time_stamp_start = 0;
+	uint32_t time_stamp_end = 0;
+	uint32_t time_elapse = 0;
+	uint32_t retry_cnt = 2;
+	uint32_t aocpu_alive;
+	uint32_t mbox_cmd;
 
-	while ((response = aml_readl32(mboxsts)))
-		;
+	time_stamp_start = read_timer_e();
+	while ((response = aml_readl32(mboxsts))) {
+		time_stamp_end = read_timer_e();
+		time_elapse = time_stamp_end - time_stamp_start;
+		/* Set mailbox wait time out: 1 second */
+		if (time_elapse > 1) {
+			printf("%s: warning: mailbox wait time out, send fail!\n",
+				__func__);
+			aocpu_alive = is_aocpu_alive();
+			if (aocpu_alive)
+				printf("%s: aocpu is alive!\n", __func__);
+			else
+				printf("%s: aocpu is not alive!\n", __func__);
+
+			if (retry_cnt == 0)
+				break;
+			retry_cnt--;
+
+			printf("%s: mailbox send retry\n", __func__);
+			mbox_cmd = MHU_CMD_BUILD(command, size + MHU_DATA_OFFSET);
+			aml_writel32(0, mboxset);
+			aml_writel32(mbox_cmd, mboxset);
+			time_stamp_start = read_timer_e();
+		}
+	}
 
 	return response;
 }
 
 void mhu_message_end(uint32_t mboxwr, uint32_t mboxirqclr, uint32_t mboxid)
 {
-	uint64_t mask = 0;
-	uint32_t lmask = 0, hmask = 0;
-
 	/* Clear all datas in mailbox buffer */
 	mbclean(mboxwr, MHU_PAYLOAD_SIZE);
 	/* Clear irq_clr */
-	mask = MHU_ACK_MASK(mboxid);
-	lmask = mask & 0xffffffff;
-	hmask = (mask >> 32) & 0xffffffff;
-	aml_writel32(lmask, REE2AO_IRQCLR_ADDR);
-	aml_writel32(hmask, REE2AO_IRQCLR_ADDR1);
+	aml_writel32(MHU_ACK_MASK(mboxid), mboxirqclr);
 }
 
 void mhu_init(void)
@@ -181,7 +228,7 @@ int scpi_send_data(uint32_t chan, uint32_t command,
 	if (sendmessage && sendsize != 0)
 		mhu_build_payload(mboxwr, sendmessage, sendsize);
 	mhu_message_send(mboxset, command, sendsize);
-	mhu_message_wait(mboxsts);
+	mhu_message_wait(mboxset, mboxsts, command, sendsize);
 	if (revmessage && revsize != 0)
 		mhu_get_payload(mboxwr, mboxrd, revmessage, revsize);
 	mhu_message_end(mboxwr, mboxirqclr, mboxid);

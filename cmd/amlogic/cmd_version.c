2baa1f87741b7788dc527376f9b70e1282d74685
bangzheng.liu <bangzheng.liu@amlogic.com>
2024-03-27 17:11:53 +0800

    bl33: uboot 2019 fix uboot startup fail [1/1]
    PD#SWPL-160577
    
    Problem:
    memset size may overflow because of strlen which change
    other pointer addr and cause exception when pointer is
    freed.
    
    Solution:
    use array absolute length for memset.
    
    Verify:
    t3x_bc302
    
    Change-Id: I81ed72d0ec854a2171d0929003e3c0a544ac8973
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

--- cmd/amlogic/cmd_version.c
+++ cmd/amlogic/cmd_version.c
@@ -23,9 +23,11 @@ const char __weak version_string[] = U_BOOT_VERSION_STRING;
 #include <asm/arch/cpu.h>
 #define GET_BL30_VERSION_SIZE 0x30303000
 #define GET_BL30_VERSION_INFO 0x30303001
+#define GET_BL30_LEN_MAX 400
 #define GET_BL30_LEN_ONCE MAILBOX_USER_DATA_SIZE
 int bl30_version_flag, bl30_version_size;
-char bl30_version_str[GET_BL30_LEN_ONCE];
+char bl30_version_str[GET_BL30_LEN_MAX];
+char bl30_version_str_once[GET_BL30_LEN_ONCE];
 #endif
 
 #ifdef CONFIG_BL30_VERSION_RTOSSDK
@@ -144,17 +146,20 @@ int get_bootloader_build_message(void)
 			//putin_buf(build_info->bl30_message.ver_str);
 			//putin_buf(",");
 #ifdef CONFIG_BL30_VERSION_SAVE
+			bl30_version_flag = GET_BL30_VERSION_INFO;
 			for (int i = 0; i < (bl30_version_size / GET_BL30_LEN_ONCE + 1); i++) {
-				bl30_version_flag = GET_BL30_VERSION_INFO;
 				if (!scpi_send_data(AOCPU_REE_CHANNEL, CMD_GET_BL30_VERSION,
-				    &bl30_version_flag, 4, &bl30_version_str, GET_BL30_LEN_ONCE)) {
-					putin_buf(bl30_version_str);
-					memset(bl30_version_str, 0, strlen(bl30_version_str));
+				    &bl30_version_flag, 4, &bl30_version_str_once,
+				    GET_BL30_LEN_ONCE)) {
+					memcpy(&bl30_version_str[i * GET_BL30_LEN_ONCE],
+					bl30_version_str_once, GET_BL30_LEN_ONCE);
+					memset(bl30_version_str_once, 0, GET_BL30_LEN_ONCE);
 				} else {
 					printf("bl30 version message get fail\n");
 					break;
 				}
 			}
+			putin_buf(bl30_version_str);
 #else
 			putin_buf(build_info->bl30_message.hash);
 			putin_buf(",");

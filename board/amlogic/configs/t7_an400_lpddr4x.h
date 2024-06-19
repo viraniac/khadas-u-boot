dfa3c9c592a52185f4435c3a42a95b32b43cd834
yi.zhang1 <yi.zhang1@amlogic.com>
2024-06-19 09:46:45 +0800

    scs: t7 support scs [2/5]
    PD#SWPL-172150
    
    Problem:
    t7 support scs.
    
    Solution:
    t7 support scs.
    
    Verify:
    local
    
    Signed-off-by: yi.zhang1 <yi.zhang1@amlogic.com>
    Change-Id: Idb8fd814beb644d39f13519b7a6b8f8c9970a64d

--- board/amlogic/configs/t7_an400_lpddr4x.h
+++ board/amlogic/configs/t7_an400_lpddr4x.h
@@ -613,7 +613,10 @@
 
 /* unify build for generate encrypted bootloader "u-boot.bin.encrypt" */
 #define CONFIG_AML_CRYPTO_UBOOT   1
-//#define CONFIG_AML_SIGNED_UBOOT   1
+
+#ifdef CONFIG_SCS
+    #define CONFIG_AML_SIGNED_UBOOT   1
+#endif
 /* unify build for generate encrypted kernel image
    SRC : "board/amlogic/(board)/boot.img"
    DST : "fip/boot.img.encrypt" */
